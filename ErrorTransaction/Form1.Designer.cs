c7955ab3c5d3cb72ed235c27cae5a2d6f79213cf
MoongilProjects <43730993+MoongilProjects@users.noreply.github.com>
2018-10-28 03:34:10 -0400

    Add files via upload

---
+++ ErrorTransaction/Form1.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace ErrorTransaction
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.lblDateTime = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.rdlResolve = new System.Windows.Forms.RadioButton();
+            this.rdlError = new System.Windows.Forms.RadioButton();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnPrevious = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.lblPageNumber = new System.Windows.Forms.Label();
+            this.lblTotalCount = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 9.899889F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 90.10011F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 189F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lblDate, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lblDateTime, 3, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(964, 56);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Location = new System.Drawing.Point(283, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(195, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Error Transaction";
+            // 
+            // lblDate
+            // 
+            this.lblDate.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.lblDate.AutoSize = true;
+            this.lblDate.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDate.Location = new System.Drawing.Point(698, 18);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(73, 19);
+            this.lblDate.TabIndex = 1;
+            this.lblDate.Text = "DateTime";
+            // 
+            // lblDateTime
+            // 
+            this.lblDateTime.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.lblDateTime.AutoSize = true;
+            this.lblDateTime.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDateTime.Location = new System.Drawing.Point(777, 18);
+            this.lblDateTime.Name = "lblDateTime";
+            this.lblDateTime.Size = new System.Drawing.Size(73, 19);
+            this.lblDateTime.TabIndex = 2;
+            this.lblDateTime.Text = "DateTime";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // rdlResolve
+            // 
+            this.rdlResolve.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.rdlResolve.AutoSize = true;
+            this.rdlResolve.Location = new System.Drawing.Point(98, 19);
+            this.rdlResolve.Name = "rdlResolve";
+            this.rdlResolve.Size = new System.Drawing.Size(83, 17);
+            this.rdlResolve.TabIndex = 1;
+            this.rdlResolve.TabStop = true;
+            this.rdlResolve.Text = "Resolve List";
+            this.rdlResolve.UseVisualStyleBackColor = true;
+            // 
+            // rdlError
+            // 
+            this.rdlError.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.rdlError.AutoSize = true;
+            this.rdlError.Location = new System.Drawing.Point(9, 19);
+            this.rdlError.Name = "rdlError";
+            this.rdlError.Size = new System.Drawing.Size(66, 17);
+            this.rdlError.TabIndex = 0;
+            this.rdlError.TabStop = true;
+            this.rdlError.Text = "Error List";
+            this.rdlError.UseVisualStyleBackColor = true;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSearch.Location = new System.Drawing.Point(540, 18);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(205, 20);
+            this.txtSearch.TabIndex = 2;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnSearch.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.Location = new System.Drawing.Point(436, 16);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = false;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.tableLayoutPanel3.ColumnCount = 5;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 43.7299F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 56.2701F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 216F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 126F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 210F));
+            this.tableLayoutPanel3.Controls.Add(this.rdlResolve, 1, 0);
+            this.tableLayoutPanel3.Controls.Add(this.rdlError, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.txtSearch, 4, 0);
+            this.tableLayoutPanel3.Controls.Add(this.btnSearch, 3, 0);
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(117, 94);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(748, 56);
+            this.tableLayoutPanel3.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.HighlightText;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 184);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(964, 180);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel2.ColumnCount = 5;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 62F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 62F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 264F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 186F));
+            this.tableLayoutPanel2.Controls.Add(this.btnPrevious, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.btnNext, 2, 0);
+            this.tableLayoutPanel2.Controls.Add(this.lblPageNumber, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.lblTotalCount, 4, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(313, 436);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(663, 44);
+            this.tableLayoutPanel2.TabIndex = 4;
+            this.tableLayoutPanel2.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel2_Paint);
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnPrevious.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnPrevious.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPrevious.Location = new System.Drawing.Point(7, 3);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(47, 38);
+            this.btnPrevious.TabIndex = 0;
+            this.btnPrevious.Text = "<<";
+            this.btnPrevious.UseVisualStyleBackColor = false;
+            this.btnPrevious.Click += new System.EventHandler(this.btnPrevious_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnNext.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnNext.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNext.Location = new System.Drawing.Point(158, 3);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(47, 38);
+            this.btnNext.TabIndex = 1;
+            this.btnNext.Text = ">>";
+            this.btnNext.UseVisualStyleBackColor = false;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // lblPageNumber
+            // 
+            this.lblPageNumber.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblPageNumber.AutoSize = true;
+            this.lblPageNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPageNumber.Location = new System.Drawing.Point(67, 15);
+            this.lblPageNumber.Name = "lblPageNumber";
+            this.lblPageNumber.Size = new System.Drawing.Size(79, 13);
+            this.lblPageNumber.TabIndex = 2;
+            this.lblPageNumber.Text = "PageNumber";
+            // 
+            // lblTotalCount
+            // 
+            this.lblTotalCount.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblTotalCount.AutoSize = true;
+            this.lblTotalCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalCount.Location = new System.Drawing.Point(549, 15);
+            this.lblTotalCount.Name = "lblTotalCount";
+            this.lblTotalCount.Size = new System.Drawing.Size(41, 13);
+            this.lblTotalCount.TabIndex = 3;
+            this.lblTotalCount.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(988, 551);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.tableLayoutPanel3);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.Label lblDateTime;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.RadioButton rdlResolve;
+        private System.Windows.Forms.RadioButton rdlError;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button btnPrevious;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Label lblPageNumber;
+        private System.Windows.Forms.Label lblTotalCount;
+    }
+}
+
---
+++ ErrorTransaction/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace ErrorTransaction
+{
+    public partial class Form1 : Form
+    {
+        SqlDataAdapter da;
+        int pageNumber = 0;
+        Int64 totalRows = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Start();
+            lblDateTime.Text = DateTime.Now.ToString();
+            rdlError.Checked = true;
+            DisplayData();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblDateTime.Text = DateTime.Now.ToString();
+            timer1.Start();
+        }
+        private void DisplayData(int displayLength = 2, int displayStart = 0, int sortCol = 5, string sortDir = "desc")
+        {
+            try
+            {
+
+                DataTable dt = new DataTable();
+                using (SqlConnection con = new SqlConnection("Data Source=LAPTOP-982TEVDR\\MSSQLSERVER01;Initial Catalog=StudentDataBase;Integrated Security=true;"))
+                {
+                    using (SqlCommand command = new SqlCommand("sp_GetErrorTransactionDetails", con))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.Parameters.Add("@DisplayLength", SqlDbType.Int).Value = displayLength;
+                        command.Parameters.Add("@DisplayStart", SqlDbType.Int).Value = displayStart;
+                        command.Parameters.Add("@SortCol", SqlDbType.Int).Value = sortCol;
+                        command.Parameters.Add("@SortDir", SqlDbType.NVarChar).Value = sortDir;
+                        command.Parameters.Add("@Search", SqlDbType.NVarChar).Value = txtSearch.Text.Trim();
+                        command.Parameters.Add("@IsResolved", SqlDbType.BigInt).Value = rdlResolve.Checked;
+                        con.Open();
+                        da = new SqlDataAdapter();
+                        da.SelectCommand = command;
+                        da.Fill(dt);
+                        dataGridView1.DataSource = dt;
+                        con.Close();
+                        if (dt.Rows.Count > 0)
+                        {
+                            totalRows = Convert.ToInt64(dt.Rows[0]["TotalCount"]);
+
+                            lblTotalCount.Text = string.Format("Total Records : {0}", totalRows);
+                            Int64 divNext = 1;
+                            if (totalRows > displayLength)
+                            {
+                                divNext = (totalRows / 2);
+                            }
+
+                            lblPageNumber.Text = string.Format("page {0}/{1}", pageNumber + 1, divNext);
+                            // dt.Columns.Remove("RowNum
+
+
+                            if (divNext == 0)
+                            {
+                                btnNext.Enabled = false;
+                                btnPrevious.Enabled = false;
+                            }
+                            else
+                            {
+                                if (totalRows > displayLength && pageNumber != 0)
+                                {
+                                    if (divNext == pageNumber + 1)
+                                    {
+                                        btnNext.Enabled = false;
+                                        if (pageNumber != 0)
+                                        {
+                                            btnPrevious.Enabled = true;
+                                        }
+                                    }
+                                    else
+                                    {
+                                        btnNext.Enabled = true;
+                                    }
+                                }
+                                else
+                                {
+                                    btnPrevious.Enabled = false;
+                                    if (pageNumber + 1 != divNext)
+                                    {
+                                        btnNext.Enabled = true;
+                                    }
+                                }
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("No Record Found");
+                            txtSearch.Focus();
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+
+        }
+
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            pageNumber = 0;
+            DisplayData(2, pageNumber);
+        }
+
+        private void btnPrevious_Click(object sender, EventArgs e)
+        {
+            DisplayData(2, pageNumber--);
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            DisplayData(2, pageNumber++);
+        }
+
+        private void tableLayoutPanel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
